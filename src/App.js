4b6bf73791ee134624e0ff958472d2df1b66598f
pankaj5252 <pbkale25@gmail.com>
2024-06-14 10:17:02 +0530

    Updates

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Componants/Common/Header';
+import Footer from './Componants/Common/Footer';
 import Home from './Componants/Home';
 import About from './Componants/About';
 import NotFound from './Componants/NotFound';
@@ -15,7 +16,7 @@ const App = () => {
         <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
-
+      <Footer/>
     </Router>
   );
 }
